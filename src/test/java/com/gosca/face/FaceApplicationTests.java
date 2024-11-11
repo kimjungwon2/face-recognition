014d7995ec597acedfc9dbfb17e3ae551466eb8e
jungwon <mac2001035@gmail.com>
2024-11-11 11:17:05 +0900

    INIT: 프로젝트 시작.

---
+++ src/test/java/com/gosca/face/FaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gosca.face;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
