8b0a6e5484cdb25f2f945c9868a24b68cc72b5b3
jungwon <mac2001035@gmail.com>
2024-11-16 09:25:45 +0900

    Collection User와 face 연계

--- src/main/java/com/gosca/face/service/rekognition/facecollection/CollectionFaceAdditionService.java
+++ src/main/java/com/gosca/face/service/rekognition/facecollection/CollectionFaceAdditionService.java
@@ -1,2 +1,92 @@
-package com.gosca.face.service.rekognition.facecollection;public class CollectionFaceAdditionService {
+package com.gosca.face.service.rekognition.facecollection;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.core.SdkBytes;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.rekognition.RekognitionClient;
+import software.amazon.awssdk.services.rekognition.model.IndexFacesResponse;
+import software.amazon.awssdk.services.rekognition.model.IndexFacesRequest;
+import software.amazon.awssdk.services.rekognition.model.Image;
+import software.amazon.awssdk.services.rekognition.model.QualityFilter;
+import software.amazon.awssdk.services.rekognition.model.Attribute;
+import software.amazon.awssdk.services.rekognition.model.FaceRecord;
+import software.amazon.awssdk.services.rekognition.model.UnindexedFace;
+import software.amazon.awssdk.services.rekognition.model.RekognitionException;
+import software.amazon.awssdk.services.rekognition.model.Reason;
+
+import javax.annotation.PostConstruct;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.List;
+
+@Slf4j
+@Service
+public class CollectionFaceAdditionService {
+    private RekognitionClient rekognitionClient;
+
+    @Value("${aws.credentials.accessKey}")
+    private String accessKey;
+    @Value("${aws.credentials.secretKey}")
+    private String secretKey;
+    @Value("${aws.region}")
+    private String region;
+
+    @PostConstruct
+    public void init() {
+        AwsBasicCredentials awsCreds = AwsBasicCredentials.create(accessKey, secretKey);
+
+        this.rekognitionClient = RekognitionClient.builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(awsCreds))
+                .build();
+    }
+
+
+    public void addToCollection(String collectionId, String sourceImage) {
+
+        try {
+            InputStream sourceStream = new FileInputStream(sourceImage);
+            SdkBytes sourceBytes = SdkBytes.fromInputStream(sourceStream);
+            Image souImage = Image.builder()
+                    .bytes(sourceBytes)
+                    .build();
+
+            IndexFacesRequest facesRequest = IndexFacesRequest.builder()
+                    .collectionId(collectionId)
+                    .image(souImage)
+                    .maxFaces(1)
+                    .qualityFilter(QualityFilter.AUTO)
+                    .detectionAttributes(Attribute.DEFAULT)
+                    .build();
+
+            IndexFacesResponse facesResponse = rekognitionClient.indexFaces(facesRequest);
+            log.info("Results for the image");
+            log.info("\n Faces indexed:");
+            List<FaceRecord> faceRecords = facesResponse.faceRecords();
+            for (FaceRecord faceRecord : faceRecords) {
+                log.info("  Face ID: {}", faceRecord.face().faceId());
+                log.info("  Location:{}", faceRecord.faceDetail().boundingBox().toString());
+            }
+
+            List<UnindexedFace> unindexedFaces = facesResponse.unindexedFaces();
+            log.info("Faces not indexed:");
+            for (UnindexedFace unindexedFace : unindexedFaces) {
+                log.info("  Location:{}",unindexedFace.faceDetail().boundingBox().toString());
+                log.info("  Reasons:");
+                for (Reason reason : unindexedFace.reasons()) {
+                    log.info("Reason:  {}", reason);
+                }
+            }
+
+        } catch (RekognitionException | FileNotFoundException e) {
+            log.info(e.getMessage());
+            System.exit(1);
+        }
+    }
 }
--- src/test/java/com/gosca/face/service/rekognition/facecollection/CollectionFaceAdditionServiceTest.java
+++ src/test/java/com/gosca/face/service/rekognition/facecollection/CollectionFaceAdditionServiceTest.java
@@ -1,4 +1,21 @@
+package com.gosca.face.service.rekognition.facecollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
 class CollectionFaceAdditionServiceTest {
-  
+
+    @Autowired
+    CollectionFaceAdditionService collectionFaceAdditionService;
+
+    @Test
+    void test(){
+        String sourceImage = "C:\\Users\\gram\\Desktop\\face\\src\\main\\resources\\지연.jpg";
+
+        collectionFaceAdditionService.addToCollection("GOSCA_TEST",sourceImage);
+    }
 }
