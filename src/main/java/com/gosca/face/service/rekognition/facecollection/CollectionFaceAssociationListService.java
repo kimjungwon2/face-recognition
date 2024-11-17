b6f5bf477ca4f84c97b0340eaa337ccae26adab2
jungwon <mac2001035@gmail.com>
2024-11-17 09:27:08 +0900

    Collection User 목록 조회

--- src/main/java/com/gosca/face/service/rekognition/facecollection/CollectionFaceAssociationListService.java
+++ src/main/java/com/gosca/face/service/rekognition/facecollection/CollectionFaceAssociationListService.java
@@ -1,2 +1,60 @@
-package com.gosca.face.service.rekognition.facecollection;public class CollectionFaceAssociationListService {
+package com.gosca.face.service.rekognition.facecollection;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.rekognition.RekognitionClient;
+import software.amazon.awssdk.services.rekognition.model.Face;
+import software.amazon.awssdk.services.rekognition.model.ListFacesRequest;
+import software.amazon.awssdk.services.rekognition.model.ListFacesResponse;
+import software.amazon.awssdk.services.rekognition.model.RekognitionException;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Slf4j
+@Service
+public class CollectionFaceAssociationListService {
+    private RekognitionClient rekognitionClient;
+
+    @Value("${aws.credentials.accessKey}")
+    private String accessKey;
+    @Value("${aws.credentials.secretKey}")
+    private String secretKey;
+    @Value("${aws.region}")
+    private String region;
+
+    @PostConstruct
+    public void init() {
+        AwsBasicCredentials awsCreds = AwsBasicCredentials.create(accessKey, secretKey);
+
+        this.rekognitionClient = RekognitionClient.builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(awsCreds))
+                .build();
+    }
+
+
+    public void listFacesCollection(String collectionId) {
+        try {
+            ListFacesRequest facesRequest = ListFacesRequest.builder()
+                    .collectionId(collectionId)
+                    .maxResults(10)
+                    .build();
+
+            ListFacesResponse facesResponse = rekognitionClient.listFaces(facesRequest);
+            List<Face> faces = facesResponse.faces();
+            for (Face face: faces) {
+                log.info("Confidence level there is a face: {}",face.confidence());
+                log.info("The face Id value is {}",face.faceId());
+            }
+
+        } catch (RekognitionException e) {
+            log.info(e.getMessage());
+            System.exit(1);
+        }
+    }
 }
--- src/test/java/com/gosca/face/service/rekognition/facecollection/CollectionFaceAssociationListServiceTest.java
+++ src/test/java/com/gosca/face/service/rekognition/facecollection/CollectionFaceAssociationListServiceTest.java
@@ -1,4 +1,19 @@
+package com.gosca.face.service.rekognition.facecollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
 class CollectionFaceAssociationListServiceTest {
-  
+    @Autowired
+    CollectionFaceAssociationListService collectionFaceAssociationListService;
+
+
+    @Test
+    void listFacesCollection() {
+        collectionFaceAssociationListService.listFacesCollection("GOSCA_TEST");
+    }
 }
