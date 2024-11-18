1578d37fc5341cd7fa6130ec1f66007c59208757
jungwon <mac2001035@gmail.com>
2024-11-18 09:42:29 +0900

    Collection User V1 버전으로 추가

--- src/main/java/com/gosca/face/service/rekognition/facecollection/CollectionCreateUserService.java
+++ src/main/java/com/gosca/face/service/rekognition/facecollection/CollectionCreateUserService.java
@@ -1,20 +1,48 @@
 package com.gosca.face.service.rekognition.facecollection;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
 import com.amazonaws.services.rekognition.AmazonRekognition;
 import com.amazonaws.services.rekognition.AmazonRekognitionClientBuilder;
 import com.amazonaws.services.rekognition.model.CreateUserRequest;
 import com.amazonaws.services.rekognition.model.CreateUserResult;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.rekognition.RekognitionClient;
+
+import javax.annotation.PostConstruct;
 
 
 @Slf4j
 @Service
 public class CollectionCreateUserService {
 
-    public void createUser(String collectionId, String userId) {
-        AmazonRekognition rekognitionClient = AmazonRekognitionClientBuilder.
+    private AmazonRekognition rekognitionClient;
 
+    @Value("${aws.credentials.accessKey}")
+    private String accessKey;
+    @Value("${aws.credentials.secretKey}")
+    private String secretKey;
+    @Value("${aws.region}")
+    private String region;
+
+    @PostConstruct
+    public void init() {
+        BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKey, secretKey);
+
+        this.rekognitionClient = AmazonRekognitionClientBuilder.standard()
+                .withRegion(Regions.fromName(region))
+                .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
+                .build();
+    }
+
+    public void createUser(String collectionId, String userId) {
 
         CreateUserRequest request = new CreateUserRequest()
                 .withCollectionId(collectionId)
