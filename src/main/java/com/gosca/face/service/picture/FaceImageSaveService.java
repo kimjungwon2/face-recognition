69636afb1899b18e02e1d56094f12f9196e2248e
jungwon <mac2001035@gmail.com>
2024-11-19 15:40:23 +0900

    FEAT: 이미지 저장 Service

---
+++ src/main/java/com/gosca/face/service/picture/FaceImageSaveService.java
@@ -0,0 +1,68 @@
+package com.gosca.face.service.picture;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.rekognition.AmazonRekognition;
+import com.amazonaws.services.rekognition.AmazonRekognitionClientBuilder;
+import com.gosca.face.service.rekognition.facecollection.CollectionFaceAdditionService;
+import com.gosca.face.service.rekognition.facecollection.CollectionUserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.rekognition.RekognitionClient;
+
+import javax.annotation.PostConstruct;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class FaceImageSaveService {
+
+    private final CollectionFaceAdditionService collectionFaceAdditionService;
+    private final CollectionUserService collectionUserService;
+
+
+    private RekognitionClient rekognitionClientV2;
+    private AmazonRekognition rekognitionClientV1;
+
+    @Value("${aws.credentials.accessKey}")
+    private String accessKey;
+    @Value("${aws.credentials.secretKey}")
+    private String secretKey;
+    @Value("${aws.region}")
+    private String region;
+
+    @PostConstruct
+    public void init() {
+
+        AwsBasicCredentials awsCreds = AwsBasicCredentials.create(accessKey, secretKey);
+        BasicAWSCredentials awsCreds2 = new BasicAWSCredentials(accessKey, secretKey);
+
+        this.rekognitionClientV2 = RekognitionClient.builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(awsCreds))
+                .build();
+
+        this.rekognitionClientV1 = AmazonRekognitionClientBuilder.standard()
+                .withRegion(Regions.fromName(region))
+                .withCredentials(new AWSStaticCredentialsProvider(awsCreds2))
+                .build();
+    }
+
+
+    public boolean saveUserFace(){
+
+
+
+        return true;
+    }
+
+
+
+
+}
