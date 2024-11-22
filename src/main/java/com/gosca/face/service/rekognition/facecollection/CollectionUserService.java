a17ffc988864f5339e549d83175a14fcd9c3d180
jungwon <mac2001035@gmail.com>
2024-11-22 11:25:40 +0900

    FEAT: 얼굴 연계 실패 시, 예외 처리.

--- src/main/java/com/gosca/face/service/rekognition/facecollection/CollectionUserService.java
+++ src/main/java/com/gosca/face/service/rekognition/facecollection/CollectionUserService.java
@@ -26,7 +26,7 @@ public class CollectionUserService {
             rekognitionClient.createUser(request);
 
         }catch (AmazonRekognitionException e) {
-            log.error("Amazon Rekognition 요청 실패: {}", e.getMessage());
+            log.error("Amazon Rekognition User 등록 요청 실패: {}", e.getMessage());
             if (e.getStatusCode()==400) {
                 throw new IllegalArgumentException(e.getErrorCode(), e);
             } else {
@@ -57,24 +57,33 @@ public class CollectionUserService {
     }
 
     public void associateFace(AmazonRekognition rekognitionClient, String collectionId, String faceId, String userId){
+        try {
+            log.info("Associating faces to the existing userId: {}", userId);
+            List<String> faceIds = Arrays.asList(faceId);
 
-        System.out.println("Associating faces to the existing userId: " + userId);
-        List<String> faceIds = Arrays.asList(faceId);
+            AssociateFacesRequest request = new AssociateFacesRequest()
+                    .withCollectionId(collectionId)
+                    .withUserId(userId)
+                    .withFaceIds(faceIds);
 
-        AssociateFacesRequest request = new AssociateFacesRequest()
-                .withCollectionId(collectionId)
-                .withUserId(userId)
-                .withFaceIds(faceIds);
+            AssociateFacesResult result = rekognitionClient.associateFaces(request);
 
-        AssociateFacesResult result = rekognitionClient.associateFaces(request);
+            log.info("Successful face associations: {}", result.getAssociatedFaces().size());
+            log.info("Unsuccessful face associations: {}", result.getUnsuccessfulFaceAssociations().size());
 
-        log.info("Successful face associations: {}", result.getAssociatedFaces().size());
-        log.info("Unsuccessful face associations: {}", result.getUnsuccessfulFaceAssociations().size());
-
-        if(result.getUnsuccessfulFaceAssociations().size()>=1) {
-            log.error("result:{}", result);
+            if (result.getUnsuccessfulFaceAssociations().size() >= 1) {
+                log.error("result:{}", result);
+                deleteUser(rekognitionClient, collectionId, userId);
+                throw new IllegalStateException("유저와 얼굴 연계가 안 됐습니다.");
+            }
+        } catch (AmazonRekognitionException e) {
+            log.error("associateFace 요청 실패: {}", e.getMessage());
             deleteUser(rekognitionClient, collectionId, userId);
-            throw new IllegalStateException("유저와 얼굴 연계가 안 됐습니다.");
+            if (e.getStatusCode()==400) {
+                throw new IllegalArgumentException(e.getErrorCode(), e);
+            } else {
+                throw new RuntimeException(e.getErrorCode(), e);
+            }
         }
     }
 
